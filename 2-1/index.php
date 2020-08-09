9c0a749e8a2c2b30291b3cbb301bf92f2175fa96
WAKO OHIRA <66293814+wako-ohira@users.noreply.github.com>
2020-08-09 23:15:51 +0900

    Hello World
    課題提出　PHPによる出力方法とIT用語確認

---
+++ 2-1/index.php
@@ -0,0 +1,23 @@
+<?php
+// PHPのカリキュラム2-1
+//コメントを入れたい時は→//
+?>
+<?php
+/*
+PHPのカリキュラム
+2-1
+複数行コメントを入れたい時は/と/の間に書く
+*/ ?>
+
+<html>
+    <p>I say that <?php echo "Hello World!!"; ?></p>
+</html>
+
+<?php
+echo "1.開発環境・検証環境・本番環境"."<br>";
+echo "開発環境：システムを開発する時の環境。個人のパソコンの環境のことがお多いが、チームで開発していれば複数人同士で機能を共有・統合する開発環境を用意することがある。<br>検証環境：開発し終わった機能を確認・テストするための、Testing環境。チーム規模が小さければ環境を分けずに行うことも。<br>本番環境：サービス提供の実運用を行う環境。ここでは発注したお客さんの所有物になる。"."<br>";
+echo "2.基本設計書・詳細設計書"."<br>";
+echo "基本計画書：ソフトウェアの動きを外から見たときにどのような動きをするか？（what）を決めたもの<br>詳細設計書：基本設計で決められた動きをどうやって実装するか（How）を決めたもの"."<br>";
+echo "3.Ajax"."<br>";
+echo "Ajax：（エイジャックス）JavaScriptを使って非同期通信をする実装方法の呼び方。一般的にはWebブラウザとWebサーバのやりとりだが、Javascriptが持っている、他の処理を邪魔せずに直接Webサーバとやりとりできる機能（非同期通信）を使う。"."<br>";
+?><img class="alignnone size-full wp-image-371" src="http://54.238.147.46/wp-content/uploads/2020/04/update_task_status.png" alt="" width="716" height="180" data-pagespeed-url-hash="608921261" onload="pagespeed.CriticalImages.checkImageForCriticality(this);">
