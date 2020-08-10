48f56a74d706ebf3850a4121f8d99f756619483a
WAKO OHIRA <66293814+wako-ohira@users.noreply.github.com>
2020-08-10 14:06:26 +0900

    変数とデータ型
    課題提出　変数の概要と出力方法　データ型

---
+++ 2-2/index.php
@@ -0,0 +1,66 @@
+<?php
+/*
+$message = "Hello World";
+echo $message;
+*/
+?>
+
+<?php
+/*
+$x = 8;
+$y = 5.55;
+echo $x;
+echo "<br>";
+echo $y;
+*/
+?>
+
+<?php
+//var_dumpを使うと型も表示してくれる
+/*
+$message = "Hello World!";
+var_dump($message);
+
+$x = 8;
+$y = 5.55;
+echo "<br>";
+var_dump($x);
+echo '<br>';
+echo $y;
+*/
+?>
+
+<?php
+/*
+$flag = true;
+var_dump($flag); //boolはブーリアンの略でtrue/falseをまとめて行ったりする。
+*/
+?>
+
+<?php
+/*
+//変数に何も入っていない状態を作るとき
+$emp = null;
+var_dump($emp);
+*/
+?>
+
+<?php
+/*
+//日本語は難しいから１文字３つくらいでカウントされる
+$en_words = 'Hello World';
+var_dump($en_words);
+echo '<br>';
+$ja_words = 'こんにちは';
+var_dump($ja_words); //結果：string(15)"こんにちは"
+*/
+?>
+
+<?php
+echo "1.API"."<br>";
+echo "Application Programming Interface アプリケーションに別のプログラムを追加するための窓口。例えばオンラインショップでGoogleアカウントを持っていればそれを登録してスムーズに買い物ができる。APIを使うには提供する企業サイトからAPIキーを取得する必要がある。"."<br>";
+echo "2.SVN（Subversion）"."<br>";
+echo "ファイルの変更内容を管理してくれる集中型バージョン管理システム。ローカルリポジトリは持たず、修正・変更したソースコードは、リモートリポジトリにコミットする。"."<br>";
+?>
+
+
