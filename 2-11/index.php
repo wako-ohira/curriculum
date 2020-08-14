bd1d2a5b858288ba18907f303985b1578020f713
WAKO OHIRA <66293814+wako-ohira@users.noreply.github.com>
2020-08-14 19:36:46 +0900

    フォームに入力した内容を取得する
    提出課題　ゲットとポストで取得

---
+++ 2-11/index.php
@@ -0,0 +1,20 @@
+ <form action="result.php" method="post">
+  名前:<input type="text" name="my_name" />
+  <br>
+  メールアドレス:<input type="text" name="email"/>
+  <br> 
+  パスワード：<input type="password" name="password" />
+  <br>
+  <input type="submit" value="送信" />
+</form>
+
+<?php
+echo '<br><br>';
+echo "仕様書・設計書" . '<br>';
+echo "仕様書：何を（どんなものを）作るかを説明じた資料（完成図）<br>設計書：どうやって作るかを説明した資料" . '<br><br>';
+echo "Git" . '<br>';
+echo "分散型バージョン管理システム。リモートリポジトリとローカルリポジトリがあり、編集履歴を管理できる。" . '<br><br>';
+echo "マージンツール" . '<br>';
+echo "ブランチで作成した複数の開発ラインを一つにまとめる方法。複数のものを一つにする機能。";
+
+?>
---
+++ 2-11/result.php
@@ -0,0 +1,10 @@
+<?php
+$my_name = $_GET['my_name'];
+$email = $_GET['email'];
+$password = $_GET['password'];
+?>
+<p>私の名前は、<?php echo $my_name; ?></p>
+<p>メールアドレスは、<?php echo $email; ?></p>
+<p>私のパスワードは、<?php echo $password; ?></p>
+
+
