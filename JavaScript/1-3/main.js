ae41427bc48bf0fcbff11f47cd9121ae80e6b2c4
WAKO OHIRA <66293814+wako-ohira@users.noreply.github.com>
2020-08-16 19:49:13 +0900

    条件分岐・演算子
    提出課題　条件分岐と演算子について

---
+++ JavaScript/1-3/main.js
@@ -0,0 +1,20 @@
+// a != 10
+let a = 7;
+console.log(a != 10); //true
+// b >= 10 && b < 20 && b % 2 === 0
+let b = 15;
+console.log(b >= 10 && b < 20 && b % 2 === 0)  //false
+
+let x = 10;
+if (x >= 10 && x <= 20) {
+  console.log("成功です");
+} else {
+  console.log("失敗です");
+}
+
+let y = 0
+if (y % 2 === 0) {
+  console.log("偶数です");
+} else if (y % 2 !== 0) {
+  console.log("偶数です");
+} 
