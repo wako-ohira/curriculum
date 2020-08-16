69c6dc99113b9228d511c54cd6f551f6450472be
WAKO OHIRA <66293814+wako-ohira@users.noreply.github.com>
2020-08-16 19:19:24 +0900

    変数・定数
    課題提出　変数と定数について

---
+++ JavaScript/1-2/main.js
@@ -0,0 +1,23 @@
+// 変数を定義して出力 その変更
+let name = "hoge";
+console.log(name);
+
+name = "fuga";
+console.log(name);
+
+name = "池田"
+console.log(name + "くん");
+
+let number = 10;
+
+// 池田くんは10月生まれです。と表示
+console.log(`${name}くんは${number}月生まれです`);
+
+
+console.log("理解度チェック");
+
+let price = 100;
+console.log(price * 2);
+
+price = 500;
+console.log(price * 2);
